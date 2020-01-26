f41f972a21451a02bd1595c5815054e796b412a9
Christopher Chalmers <c.chalmers@me.com>
2020-01-26 10:44:07 +0000

    Fix that shell

--- shell.nix
+++ shell.nix
@@ -3,7 +3,15 @@
 
 with nixpkgs;
 
-stdenv.mkDerivation {
+let ghcid = haskellPackages.callCabal2nix "ghcid" (
+  pkgs.fetchFromGitHub {
+    owner  = "ndmitchell";
+    repo   = "ghcid";
+    rev    = "f572318f32b1617f6054248e5888af68222f8e50";
+    sha256 = "1icg3r70lg2kmd9gdc024ih1n9nrja98yav74z9nvykqygvv5w0n";
+    }) {};
+
+in stdenv.mkDerivation {
   name = "circuit-notation-env";
 
   buildInputs = [
