7d7f9ec63b91d252c53dec31d5777d309c70d6f7
Martijn Bastiaan <martijn@hmbastiaan.nl>
2020-10-04 14:57:46 +0200

    Add test to see whether plugin compiles

---
+++ tests/unittests.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE Arrows #-}
+
+-- This option is a test by itself: if we were to export a plugin with the
+-- wrong type or name, GHC would refuse to compile this file.
+{-# OPTIONS -fplugin=CircuitNotation #-}
+
+module Main where
+
+main :: IO ()
+main = pure ()
